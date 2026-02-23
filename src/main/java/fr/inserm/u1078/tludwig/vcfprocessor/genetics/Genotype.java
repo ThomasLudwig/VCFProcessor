13820f7eb02fa1def46b247623f13473cdd3e6a9
Thomas E. Ludwig <thomas.e.ludwig@gmail.com>
2026-02-23 15:03:46 +0100

    ## 1.2.8 (2026-02-04) ### core ### functions - **added** : `GenerateCorrespondenceTable` Generate a Correspondence Table between a reference panel and an anonymized version - **added** : `ApplyCorrespondenceTable` Apply the Correspondence Table to a reference panel VCF ### filters ### graphs ### other

--- src/main/java/fr/inserm/u1078/tludwig/vcfprocessor/genetics/Genotype.java
+++ src/main/java/fr/inserm/u1078/tludwig/vcfprocessor/genetics/Genotype.java
@@ -57,16 +57,21 @@ public class Genotype {
       return null;
     String[] genos = geno.split("[/\\|]", -1); //split by / or |
     int[] all = new int[genos.length];
+    int last = 0;
     for(int i = 0 ; i < all.length; i++) {
       try {
         all[i] = Integer.parseInt(genos[i]);
-        if(all[i] == 63)
-          Message.error("Genotype error : ["+geno+"]");
+        if(all[i] < 63)
+          last = i;
       } catch(NumberFormatException e){
         Message.error("Could not get alleles from the genotype ["+geno+"]");
       }
     }
-    return all;
+
+    if(all.length == last) return all;
+    int[] sub = new int[last + 1];
+    System.arraycopy(all, 0, sub, 0, last + 1);
+    return sub;
   }
 
   public final void setTo(Genotype replacement) {
